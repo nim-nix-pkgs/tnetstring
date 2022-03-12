c749ba376831ee12f24f4cf2b49c5ee28c07176e
hugosenari <hugosenari@gmail.com>
2022-03-12 03:40:25 -0300

    chore: re index tnetstring

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Parsing and serializing for the TNetstring format.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-tnetstring-master.flake = false;
+  inputs.src-tnetstring-master.owner = "mahlonsmith";
+  inputs.src-tnetstring-master.ref   = "refs/heads/master";
+  inputs.src-tnetstring-master.repo  = "nim-tnetstring";
+  inputs.src-tnetstring-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-tnetstring-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-tnetstring-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
